6a472bbfae1f7e0c84c5f865d47658729ec8675d
unknown <karanjitrishav4@gmail.com>
2020-09-24 21:43:05 +0545

    Added main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from sklearn import neighbors, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv('car.data')
+print(data.head())
